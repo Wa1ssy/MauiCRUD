6e843c5650ca80581413de022d80e3f7ce481dc6
Opilane TTHK <brawlstarstop2289@gmail.com>
2025-02-19 11:33:45 +0200

    need  to copy from teacher

---
+++ MauiCRUD/Data/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    public class DatabaseContext
+    {
+        private const string DbName = "CRUDdb3";
+        private static string DbPath => Path.Combine(".", DbName);
+        private SQLiteAsyncConnection _connection;
+    }
+}
